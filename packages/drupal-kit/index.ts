2cff7233271e8553faea6cc1e6ddc2dbfa756833
CobyPear <coby.sher@pantheon.io>
2022-03-08 10:42:07 -0600

    DB-1681: bubble up cache-control headers

--- packages/drupal-kit/index.ts
+++ packages/drupal-kit/index.ts
@@ -2,10 +2,12 @@ import DrupalState from './src/PantheonDrupalState';
 import { fetchJsonapiEndpoint } from '@gdwc/drupal-state';
 import defaultFetch from './src/fetch/defaultFetch';
 import addSurrogateKeyHeader from './src/utils/addSurrogateKeyHeader';
+import updateMaxAge from './src/utils/updateMaxAge';
 
 export {
   DrupalState,
   fetchJsonapiEndpoint,
   defaultFetch,
   addSurrogateKeyHeader,
+  updateMaxAge,
 };
