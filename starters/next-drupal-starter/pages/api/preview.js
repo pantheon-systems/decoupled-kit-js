093942fee1dea90e5fc5ce359b8979e0e18b0c77
CobyPear <coby.sher@pantheon.io>
2022-05-09 17:04:47 -0500

    DB-1570: Update preview to validate content before redirect

--- starters/next-drupal-starter/pages/api/preview.js
+++ starters/next-drupal-starter/pages/api/preview.js
@@ -1,34 +1,56 @@
+import { globalDrupalStateAuthStores } from "../../lib/drupalStateContext";
+
 const preview = async (req, res) => {
+  console.log("req.query:", req.query);
+
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
   if (req.query.secret !== process.env.PREVIEW_SECRET || !req.query.slug) {
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(401).json({ message: "Invalid token" });
   }
 
-  // TODO - Fetch the headless CMS to check if the provided `slug` exists
-  // getPostBySlug would implement the required fetching logic to the headless CMS
-  // const post = await getPostBySlug(req.query.slug)
+  // returns the store that matches the locale found in the requested url
+  // or the only store if using a monolingual backend
+  const [store] = globalDrupalStateAuthStores.filter(({ defaultLocale }) => {
+    const regex = new RegExp(`/${defaultLocale}/`);
+    return defaultLocale ? regex.test(req.url) : true;
+  });
+
+  // get the object name from the slug
+  const regex = new RegExp(
+    `^(?:/${store.defaultLocale}/|/)(?<objectName>.*)/.*$`
+  );
+  const matches = req.query.slug.match(regex);
+
+  const objectName = matches.groups.objectName.replace(/s$/, ""); // remove plural
+
+  let content;
+  try {
+    // verify the content exists
+    content = await store.getObjectByPath({
+      objectName: `node--${objectName}`,
+      path: req.query.slug,
+    });
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
 
   // If the slug doesn't exist prevent preview mode from being enabled
-  // if (!post) {
-  //   return res.status(401).json({ message: 'Invalid slug' })
-  // }
+  if (!content) {
+    return res.status(401).json({ message: "Invalid slug" });
+  }
 
   // Enable Preview Mode by setting a cookie
   if (req.query.resourceVersionId) {
     res.setPreviewData({ resourceVersionId: req.query.resourceVersionId });
-  }
-  else if (req.query.key) {
+  } else if (req.query.key) {
     res.setPreviewData({ key: req.query.key });
-  }
-  else {
+  } else {
     res.setPreviewData({});
   }
 
-  // Redirect to the path from the fetched post
-  // Long term we shouldn't redirect to req.query.slug as that might lead to
-  // open redirect vulnerabilities
-  res.redirect(req.query.slug);
-}
+  // Redirect to the path from the fetched content
+  res.redirect(content.path.alias);
+};
 
 export default preview;
