166bff2ad1045d028e854cfcf24b76f2271b0432
CobyPear <coby.sher@pantheon.io>
2022-06-01 09:57:57 -0500

    DB-2566: Fetch articles instead of taxonomies

--- starters/next-drupal-starter/pages/examples/auth-api.js
+++ starters/next-drupal-starter/pages/examples/auth-api.js
@@ -7,7 +7,7 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
 
-export default function AuthApiExampleTemplate({ taxonomies }) {
+export default function AuthApiExampleTemplate({ articles }) {
   return (
     <Layout>
       <Head>
@@ -23,21 +23,13 @@ export default function AuthApiExampleTemplate({ taxonomies }) {
         </Link>
 
         <div className="mt-12 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-screen-lg">
-          {taxonomies.length > 0 && (
+          {articles.length > 0 && (
             <p>
-              If NextJS is able to make authorized requests to the Drupal API,
-              you will see a list of taxonomy types below:
+               NextJS was able to successfuly make an authenticated request to
+              Drupal!
             </p>
           )}
-          {taxonomies.length > 0 &&
-            taxonomies.map((taxonomy) => {
-              return (
-                <p key={taxonomy.id}>
-                  {taxonomy.name}: {taxonomy.description}
-                </p>
-              );
-            })}
-          {taxonomies.length === 0 && (
+          {articles.length === 0 && (
             <p>
               NextJS was unable to make an authorized request to the Drupal API.
               Please check your .env file to ensure that your CLIENT_ID and
@@ -50,16 +42,16 @@ export default function AuthApiExampleTemplate({ taxonomies }) {
   );
 }
 
-export async function getStaticProps({ locales, locale }) {
+export async function getStaticProps({ locale }) {
   const authstore = getCurrentLocaleStore(locale, globalDrupalStateAuthStores);
 
   authstore.params.clear();
-  const taxonomies = await authstore.getObject({
-    objectName: "taxonomy_vocabulary--taxonomy_vocabulary",
+  const articles = await authstore.getObject({
+    objectName: "node--article",
   });
   return {
     props: {
-      taxonomies: taxonomies,
+      articles,
       revalidate: 60,
     },
   };
