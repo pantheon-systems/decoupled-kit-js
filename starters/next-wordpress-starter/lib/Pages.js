d49504bb8681902ae8b97e1368e425004f224956
enZane <thunderstudios2@gmail.com>
2022-07-08 16:30:05 -0500

    DB-3181: Page Detail

--- starters/next-wordpress-starter/lib/Pages.js
+++ starters/next-wordpress-starter/lib/Pages.js
@@ -1,6 +1,27 @@
 import { gql } from "@pantheon-systems/wordpress-kit";
 import { client } from "./WordpressClient";
 
+export async function getAllPagesUri() {
+  const query = gql`
+    query AllPages {
+      pages {
+        edges {
+          node {
+            id
+            uri
+          }
+        }
+      }
+    }
+  `;
+
+  const {
+    pages: { edges },
+  } = await client.request(query);
+
+  return edges.map(({ node }) => node.uri.replaceAll("/", ""));
+}
+
 export async function getLatestPages() {
   const query = gql`
     query LatestPagesQuery {
@@ -27,4 +48,26 @@ export async function getLatestPages() {
   } = await client.request(query);
 
   return edges.map(({ node }) => node);
+}
+
+export async function getPageByUri(uri) {
+  const query = gql`
+    query PageByUriuery($uri: ID!) {
+      page(id: $uri, idType: URI) {
+        title
+        date
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+        content
+      }
+    }
+  `;
+
+  const { page } = await client.request(query, { uri });
+
+  return page;
 }
