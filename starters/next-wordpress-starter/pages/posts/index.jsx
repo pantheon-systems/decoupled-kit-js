da25a414d4fe042e78318ceeaec2ee781699e643
enZane <thunderstudios2@gmail.com>
2022-07-22 11:28:40 -0500

    DB-3465: Change js to jsx on components

--- starters/next-wordpress-starter/pages/posts/index.jsx
+++ starters/next-wordpress-starter/pages/posts/index.jsx
@@ -1,10 +1,11 @@
 import { NextSeo } from "next-seo";
 
-import PageHeader from "../../components/page-header.js";
+import PageHeader from "../../components/page-header";
 import Layout from "../../components/layout";
-import { getFooterMenu } from "../../lib/Menus.js";
-import { withGrid, PostGridItem } from "../../components/grid.js";
-import { getLatestPosts } from "../../lib/Posts.js";
+import { withGrid, PostGridItem } from "../../components/grid";
+
+import { getFooterMenu } from "../../lib/Menus";
+import { getLatestPosts } from "../../lib/Posts";
 
 export default function PostsListTemplate({ menuItems, SSRPosts }) {
   const PostGrid = withGrid(PostGridItem);
