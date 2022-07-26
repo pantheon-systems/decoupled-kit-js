43783f238455c5b83abdb158fee249e8ca27986d
enZane <thunderstudios2@gmail.com>
2022-07-26 13:57:55 -0500

    DB-3466: Add file for types in tailwindPlugin

--- packages/wordpress-kit/index.ts
+++ packages/wordpress-kit/index.ts
@@ -2,7 +2,7 @@ import GraphqlClientFactory from './src/lib/GraphqlClientFactory';
 import tailwindcssPlugin from './src/lib/tailwindcssPlugin';
 import { gql } from 'graphql-request';
 
-import type { TailwindcssConfig } from './src/lib/tailwindcssPlugin';
+import type { TailwindcssConfig } from './src/types/tailwindcssPlugin';
 
 export { GraphqlClientFactory, gql, tailwindcssPlugin };
 export type { TailwindcssConfig };
