31e9144e9ce140c33d36f2b32ef183a9130b8fbc
enZane <thunderstudios2@gmail.com>
2022-07-26 13:58:51 -0500

    DB-3466: Add Pullquote support

---
+++ .changeset/strong-glasses-carry.md
@@ -0,0 +1,5 @@
+---
+"@pantheon-systems/wordpress-kit": minor
+---
+
+Add support for Pullquote and split types for tailwind plugin
--- packages/wordpress-kit/src/lib/tailwindcssPlugin.ts
+++ packages/wordpress-kit/src/lib/tailwindcssPlugin.ts
@@ -1,178 +1,7 @@
 import plugin from 'tailwindcss/plugin';
 
 import type { Config } from 'tailwindcss';
-
-type ColorConfig = {
-  primary?: string;
-  secondary?: string;
-  tertiary?: string;
-  foreground?: string;
-  cyanBluishGray?: string;
-  palePink?: string;
-  vividRed?: string;
-  luminousVividOrange?: string;
-  luminousVividAmber?: string;
-  lighGreenCyan?: string;
-  vividGreenCyan?: string;
-  paleCyanBlue?: string;
-  vividCyanBlue?: string;
-  vividPurple?: string;
-  darkGray?: string;
-  lightGray?: string;
-  white?: string;
-};
-
-type PaddingConfig = {
-  backgroundX?: string;
-  backgroundY?: string;
-};
-
-type FontSizeConfig = {
-  '7xl'?: string;
-  '4xl'?: string;
-  xl?: string;
-  sm?: string;
-};
-
-type WordpressMapConfig = {
-  theme?: {
-    extend?: {
-      /**
-       * The colors mapped from the code editor in the WordPress admin.
-       * @example
-       * ```
-       *primary: '#0070f3',
-       *secondary: '#ff4081',
-       *darkGray: '#333',
-       *lightGray: '#fafafa',
-       *white: '#fff',
-       * ```
-       *
-       *@default
-       * ```
-       *primary: value of "blue-500" or '#0073a8',
-       *secondary: value of "gray-500" or '#005075',
-       *darkGray: value of "neutral-800" or '#333333',
-       *lightGray: value of "neutral-500" or '#666666',
-       *white: value of "white" or '#ffffff',
-       * ```
-       */
-      colors?: ColorConfig;
-      /**
-       * Padding applied when the block has a background color.
-       * @example
-       * ```
-       *backgroundX: '2rem',
-       *backgroundY: '1rem',
-       * ```
-       *
-       * @default
-       * ```
-       * backgroundX: value of p-5 or '1.25rem',
-       * backgroundY: value of p-9 or '2.35rem',
-       * ```
-       */
-      padding?: PaddingConfig;
-      /**
-       * The font size map for the code editor in the WordPress admin.
-       *
-       * ```
-       * huge: text-7xl
-       * large: text-4xl
-       * normal: text-xl
-       * small: text-sm
-       *```
-       *
-       * @example
-       * ```
-       *"7xl": 5rem;
-       *"4xl": 4rem;
-       *xl: 2rem;
-       *sm: 1rem;
-       * ```
-       *
-       * @default
-       * ```
-       * "7xl": '5rem',
-       * "4xl": '4rem',
-       * xl: '1.25rem',
-       * sm: '1rem',
-       */
-      fontSize?: FontSizeConfig;
-    };
-    /**
-     * The colors mapped from the code editor in the WordPress admin.
-     * @example
-     * ```
-     *primary: '#0070f3',
-     *secondary: '#ff4081',
-     *darkGray: '#333',
-     *lightGray: '#fafafa',
-     *white: '#fff',
-     * ```
-     *
-     *@default
-     * ```
-     *primary: value of "blue-500" or '#0073a8',
-     *secondary: value of "gray-500" or '#005075',
-     *darkGray: value of "neutral-800" or '#333333',
-     *lightGray: value of "neutral-500" or '#666666',
-     *white: value of "white" or '#ffffff',
-     * ```
-     */
-    colors?: ColorConfig;
-    /**
-     * Padding applied when the block has a background color.
-     * @example
-     * ```
-     *backgroundX: '2rem',
-     *backgroundY: '1rem',
-     * ```
-     *
-     * @default
-     * ```
-     * backgroundX: value of p-5 or '1.25rem',
-     * backgroundY: value of p-9 or '2.35rem',
-     * ```
-     */
-    padding?: PaddingConfig;
-    /**
-     * The font size map for the code editor in the WordPress admin.
-     *
-     * ```
-     * huge: text-7xl
-     * large: text-4xl
-     * normal: text-xl
-     * small: text-sm
-     *```
-     *
-     * @example
-     * ```
-     *"7xl": 5rem;
-     *"4xl": 4rem;
-     *xl: 2rem;
-     *sm: 1rem;
-     * ```
-     *
-     * @default
-     * ```
-     * "7xl": '5rem',
-     * "4xl": '4rem',
-     * xl: '1.25rem',
-     * sm: '1rem',
-     */
-    fontSize?: FontSizeConfig;
-  };
-};
-
-export type TailwindcssConfig = Config & WordpressMapConfig;
-
-type Color = {
-  name: string;
-  themeName: string;
-  tailwindDefault: string;
-  hexDefault: string;
-};
+import { Color, FontSize } from '../types/tailwindcssPlugin';
 
 const colorList: Color[] = [
   {
@@ -286,7 +115,7 @@ const colorList: Color[] = [
   },
 ];
 
-const fontSizeList = [
+const fontSizeList: FontSize[] = [
   {
     name: 'huge',
     tailwind: '7xl',
@@ -321,6 +150,10 @@ const backgroundColorClasses = colorList.map(
   color => `.has-${color.name}-background-color`
 );
 
+const borderColorClasses = colorList.map(
+  color => `.has-${color.name}-border-color`
+);
+
 const fontSizeClasses = fontSizeList.map(
   fontSize => `.has-${fontSize.name}-font-size`
 );
@@ -329,6 +162,7 @@ const mergeToConfig: Config = {
   content: [],
   safelist: [
     ...colorClasses,
+    ...borderColorClasses,
     '.has-background',
     ...backgroundColorClasses,
     ...fontSizeClasses,
@@ -337,18 +171,22 @@ const mergeToConfig: Config = {
     '.wp-block-quote',
     '.is-style-plain',
     '.is-style-large',
+    '.wp-block-pullquote',
   ],
 };
 
 export default plugin(function ({ addUtilities, theme }) {
+  const getColor = (color: Color) =>
+    theme(
+      `colors.${color.themeName}`,
+      theme(`colors.${color.tailwindDefault}`, color.hexDefault)
+    );
+
   const colorUtilities = colorList.reduce(
     (acc, color) => ({
       ...acc,
       [`.has-${color.name}-color`]: {
-        color:
-          theme(`colors.${color.themeName}`) ||
-          theme(`colors.${color.tailwindDefault}`) ||
-          color.hexDefault,
+        color: getColor(color),
       },
     }),
     {}
@@ -376,10 +214,7 @@ export default plugin(function ({ addUtilities, theme }) {
     (acc, color) => ({
       ...acc,
       [`.has-${color.name}-background-color`]: {
-        backgroundColor:
-          theme(`colors.${color.themeName}`) ||
-          theme(`colors.${color.tailwindDefault}`) ||
-          color.hexDefault,
+        backgroundColor: getColor(color),
       },
     }),
     {
@@ -433,11 +268,48 @@ export default plugin(function ({ addUtilities, theme }) {
     },
   };
 
+  const pullQuoteUtilities = {
+    '.wp-block-pullquote': {
+      blockquote: {
+        p: {
+          fontSize: theme('fontSize.4xl', '2.5rem'),
+        },
+        cite: {
+          textTransform: 'uppercase',
+          fontSize: theme('fontSize.xl', '1.5rem'),
+        },
+        border: 'none',
+        color: 'inherit',
+        fontStyle: 'normal',
+        quotes: 'none',
+      },
+      borderColor: 'currentColor',
+      borderWidth: '3px 0',
+      marginBottom: '0',
+      marginTop: '0',
+      padding: '2em 0',
+      textAlign: 'center',
+    },
+  };
+
+  const borderColorUtilities = colorList.reduce(
+    (acc, color) => ({
+      ...acc,
+      [`.has-${color.name}-border-color`]: {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        borderColor: `${getColor(color)} !important`,
+      },
+    }),
+    {}
+  );
+
   addUtilities([
-    colorUtilities,
-    fontSizeUtilities,
     backgroundUtilities,
+    borderColorUtilities,
+    colorUtilities,
     dropCapUtilities,
+    fontSizeUtilities,
+    pullQuoteUtilities,
     quoteUtilities,
   ]);
 }, mergeToConfig);
