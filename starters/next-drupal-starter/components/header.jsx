1966099c73e6e3fec1d26f524c1c7a2d54c1c9fa
Brian Perry <brian.perry@pantheon.io>
2022-08-11 10:49:04 -0500

    DB-3594: added ISR example

--- starters/next-drupal-starter/components/header.jsx
+++ starters/next-drupal-starter/components/header.jsx
@@ -9,6 +9,7 @@ export default function Header() {
             ["🏠 Home", "/"],
             ["📰 Articles", "/articles"],
             ["📑 Pages", "/pages"],
+            ["⚛️ Examples", "/examples"],
           ].map(([title, href]) => (
             <li className={`${href === "/" ? "mr-auto" : "mx-4"}`} key={href}>
               <Link className="font-sans" href={href}>
