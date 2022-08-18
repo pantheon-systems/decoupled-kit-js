b0a1a0748293abd1af71817929b206493b67dacc
mitchellmarkoff <mitchell.markoff@pantheon.io>
2022-08-18 12:33:19 -0700

    [DB-3547] Added comments, removed testLib

--- packages/nextjs-kit/src/index.ts
+++ packages/nextjs-kit/src/index.ts
@@ -1,8 +1,7 @@
 // entrypoint for the umd rollup build - note the default exports
 import Test from './components/test';
 import Test2 from './components/test2';
-import testLib from './lib/testLib';
 import sortChar from './lib/sortChar';
 import sortDate from './lib/sortDate';
 
-export default { Test, Test2, testLib, sortChar, sortDate };
+export default { Test, Test2, sortChar, sortDate };
--- packages/nextjs-kit/src/lib/sortChar.ts
+++ packages/nextjs-kit/src/lib/sortChar.ts
@@ -1,3 +1,16 @@
+/**
+ *
+ * @param {Array<Record<string, string | number>>} obj object to sort
+ * @param {string} key to sort object by
+ * @param {string} direction to sort object by
+ * @returns {Array<Record<string, string | number>>} the sorted object that was passed in
+ * @remarks
+ * This is a function to sort any character object on a specific key in a direction of the users choice.
+ *
+ * Note:
+ * There is a named and default export so that we can
+ * support submodules for the ESModule build and a single entrypoint for the UMD build
+ */
 export const sortChar = (
   obj: Array<Record<string, string | number>>,
   key: string,
