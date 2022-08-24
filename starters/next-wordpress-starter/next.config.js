5a8b224635148393dd6ef6712cd58f30a8887081
CobyPear <coby.sher@pantheon.io>
2022-08-24 13:41:34 -0500

    DB-3566: Add helpful error messages when critical env vars are not set

--- starters/next-wordpress-starter/next.config.js
+++ starters/next-wordpress-starter/next.config.js
@@ -6,6 +6,19 @@ require("dotenv").config({
   path: path.resolve(process.cwd(), ".env.development.local"),
 });
 
+if (
+  process.env.WPGRAPHQL_URL === undefined &&
+  process.env.PANTHEON_CMS_ENDPOINT === undefined
+) {
+  let message;
+  if (process.env.NODE_ENV === "development") {
+    message = `No WPGRAPHQL_URL found.\nSee the README.md for information on setting this variable locally.`;
+  } else if (process.env.NODE_ENV === "production") {
+    message = `No CMS Endpoint found.\nLink a CMS or set the WPGRAPHQL_URL environment variable in the settings tab in the dashboard\nIf your site does not require a backend to build, remove this check from the next.config.js.`;
+  }
+  throw new Error(message);
+}
+
 let backendUrl, imageDomain;
 if (process.env.WPGRAPHQL_URL === undefined) {
   backendUrl = `https://${process.env.PANTHEON_CMS_ENDPOINT}/wp/graphql`;
