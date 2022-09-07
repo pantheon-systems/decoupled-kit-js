3fccbfca62838d1e3e920a955309d0935ccccb76
enZane <thunderstudios2@gmail.com>
2022-09-07 13:22:26 -0500

    DB-3493: Add smooth transition to open/close ribbon

--- starters/next-drupal-starter/components/layout.jsx
+++ starters/next-drupal-starter/components/layout.jsx
@@ -1,11 +1,11 @@
 import Footer from "./footer";
 import Header from "./header";
-import PreviewRibbion from "./preview-ribbon";
+import PreviewRibbon from "./preview-ribbon";
 
 export default function Layout({ children, footerMenu, preview = false }) {
   return (
     <div className="min-h-screen max-h-screen min-w-screen max-w-screen flex flex-col">
-      {preview && <PreviewRibbion />}
+      {preview && <PreviewRibbon />}
       <Header />
       <main className="mb-auto">{children}</main>
       <Footer menuData={footerMenu} />
