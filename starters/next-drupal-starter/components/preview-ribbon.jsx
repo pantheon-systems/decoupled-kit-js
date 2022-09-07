1e951e5c53d5b8e77820505c5f2e9664e3f6204f
CobyPear <coby.sher@pantheon.io>
2022-09-07 16:36:09 -0500

    DB-3493: Use position:absolute for button

--- starters/next-drupal-starter/components/preview-ribbon.jsx
+++ starters/next-drupal-starter/components/preview-ribbon.jsx
@@ -1,37 +1,38 @@
 import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function PreviewRibbon() {
   const [show, setShow] = useState(true);
-  const [showText, setShowText] = useState(true);
 
   return (
-    <div
-      onTransitionEndCapture={(transition) => {
-        if (transition.propertyName === "transform") {
-          show ? setTimeout(() => setShowText(true), 150) : setShowText(false);
-        }
-      }}
-      className={`overflow-hidden flex justify-between items-center min-h-[3.5rem] h-14 px-8 py-2 z-10 bg-[#3017A1] text-white transition-all delay-150 w-full ${
-        !show ? "justify-start translate-x-[calc(100%-5rem)] pl-4" : ""
-      }`}
-    >
-      {showText && (
-        <>
-          <span className="mr-auto self-center">Preview Mode Enabled</span>
-          <a
-            className="justify-self-end border text-black border-black w-fit max-w- px-4 py-2 mr-12 bg-yellow-300"
-            href="/api/clear-preview"
-          >
-            Exit Preview Mode
-          </a>
-        </>
-      )}
-      <button
-        className={`self-center delay-200 ${showText ? "self-end" : ""}`}
-        onClick={() => setShow(!show)}
+    <div className="overflow-hidden">
+      <div
+        className={`flex justify-between items-center min-h-[3.5rem] h-14 px-8 py-2 bg-[#3017A1] text-white transition-all duration-300 w-full ${
+          !show ? "justify-start translate-x-[calc(100%-5rem)] pl-4" : ""
+        }`}
       >
-        <img
-          className={`w-8 transition-all ${show ? "rotate-180" : ""}`}
+        {show && (
+          <>
+            <span className="mr-auto self-center">Preview Mode Enabled</span>
+            <Link href="/api/clear-preview">
+              <a className="justify-self-end border text-black border-black w-fit max-w- px-4 py-2 mr-12 bg-yellow-300">
+                Exit Preview Mode
+              </a>
+            </Link>
+          </>
+        )}
+      </div>
+      <button
+        className={`absolute right-6 top-[.75rem] z-10`}
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        <Image
+          className={`transition-all ${show ? "rotate-180" : ""}`}
+          height={32}
+          width={32}
           src="/collapse.svg"
         />
       </button>
