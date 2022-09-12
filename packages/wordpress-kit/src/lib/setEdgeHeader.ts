7059da1ba36840cd6dd0eb93f97027a4e48678c6
Brian Perry <brian.perry@pantheon.io>
2022-09-12 10:53:14 -0500

    DB-3822: update typedocs and add tests

--- packages/wordpress-kit/src/lib/setEdgeHeader.ts
+++ packages/wordpress-kit/src/lib/setEdgeHeader.ts
@@ -3,12 +3,17 @@ import { ServerResponse } from 'http';
 const defaultCacheControlValue =
 	'public, s-maxage=10, stale-while-revalidate=600';
 
+/**
+ * Sets response headers for edge caching.
+ * @param options.res response object
+ * @param options.cacheControl optional value to override cache control header, defaults to 'public, s-maxage=10, stale-while-revalidate=600'
+ */
 const setEdgeHeader = ({
 	res,
 	cacheControl = defaultCacheControlValue,
 }: {
 	res: ServerResponse;
-	cacheControl: string;
+	cacheControl?: string;
 }) => {
 	res.setHeader('Cache-Control', cacheControl);
 };
