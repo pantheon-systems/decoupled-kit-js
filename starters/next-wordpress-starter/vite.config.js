9342fb24c0cee962e7c82b14070955b5b12de363
CobyPear <coby.sher@pantheon.io>
2022-09-13 13:02:46 -0500

    DB-3767:
    - Add vitest dependencies to next-wp-starter
    - Update package-lock.json
    - Add first test

---
+++ starters/next-wordpress-starter/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+	return {
+		test: {
+			globals: true,
+			// setupFiles: ['__tests__/setupFile.js'],
+			coverage: {
+				reportsDirectory: `./coverage`,
+			},
+		},
+		plugins: [react()],
+		define: {
+			'process.env.WP_APPLICATION_USERNAME': JSON.stringify(`decoupled_user`),
+			'process.env.WP_APPLICATION_PASSWORD': JSON.stringify(
+				'abcd efgh ijkl mnop',
+			),
+		},
+	};
+});
