e314ff5d7e62856bb25174469ef38596b6f59ea0
CobyPear <coby.sher@pantheon.io>
2022-09-14 09:17:06 -0500

    DB-3767: Clean up vite.config.js

--- starters/next-wordpress-starter/vite.config.js
+++ starters/next-wordpress-starter/vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig(({ mode }) => {
 	return {
 		test: {
 			globals: true,
-			// setupFiles: ['__tests__/setupFile.js'],
 			coverage: {
 				reportsDirectory: `./coverage`,
 			},
@@ -14,10 +13,6 @@ export default defineConfig(({ mode }) => {
 		define: {
 			'process.env.backendUrl': JSON.stringify(
 				'https://my-wordpress-site.pantheon.io/wp/graphql',
-			),
-			'process.env.WP_APPLICATION_USERNAME': JSON.stringify(`decoupled_user`),
-			'process.env.WP_APPLICATION_PASSWORD': JSON.stringify(
-				'abcd efgh ijkl mnop',
 			),
 		},
 	};
