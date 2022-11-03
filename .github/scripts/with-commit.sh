3e34ffc03a7f114b4e0213da2c0a9c1c4d0fd20d
CobyPear <coby.sher@pantheon.io>
2022-11-03 08:56:25 -0500

    DB-4593: Change git user to Pantheon Automation

--- .github/scripts/with-commit.sh
+++ .github/scripts/with-commit.sh
@@ -1,4 +1,4 @@
-git config --local user.name "GitHub Action"
-git config --local user.email "action@github.com"
+git config --global user.email "bot@getpantheon.com"
+git config --global user.name "Pantheon Automation"
 git commit -am "[skip ci] $COMMIT_MESSAGE on $(date +"%D at %T %Z")"
 git push
