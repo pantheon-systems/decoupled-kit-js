94efc8347f60d1d0639978a6252251c2d4700b8f
mitchellmarkoff <mitchell.markoff@pantheon.io>
2022-11-03 16:40:52 -0700

    DB-4065: Block separator styles

---
+++ packages/wordpress-kit/src/lib/tailwindcssPlugin/components/Separator.ts
@@ -0,0 +1,34 @@
+export const SeparatorComponent = () => {
+	return {
+		'.wp-block-separator': {
+			border: 'none',
+			borderBottom: '2px solid',
+			marginLeft: 'auto',
+			marginRight: 'auto',
+			borderColor: 'black !important',
+			'&.is-style-default': {
+				width: '100px',
+			},
+			'&.is-style-wide': {
+				maxWidth: '650px',
+				marginLeft: 'auto !important',
+				marginRight: 'auto !important',
+			},
+			'&.is-style-dots': {
+				background: 'none!important',
+				border: 'none',
+				textAlign: 'center',
+				lineHeight: '1',
+				height: 'auto',
+			},
+			'&.is-style-dots:before': {
+				content: '"···"',
+				color: 'currentColor',
+				fontSize: '1.5em',
+				letterSpacing: '2em',
+				paddingLeft: '2em',
+				fontFamily: 'serif',
+			},
+		},
+	};
+};
