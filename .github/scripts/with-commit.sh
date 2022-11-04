0b22dc50243aafe1b463e00530855b0db3c81894
CobyPear <coby.sher@pantheon.io>
2022-11-04 16:17:12 -0500

    Revert "Merge pull request #374 from CobyPear/DB-4593-commit-in-ci-script"
    This reverts commit 5bf03f873725afc236ca3dba65824eba9973b99f, reversing
    changes made to e28cf0d7bcef73995d50404f7e0b5f53b7ac4ce6.

--- .github/scripts/with-commit.sh
+++
@@ -1,4 +0,0 @@
-git config --global user.email "bot@getpantheon.com"
-git config --global user.name "Pantheon Automation"
-git commit -am "[skip ci] $COMMIT_MESSAGE on $(date +"%D at %T %Z")"
-git push
