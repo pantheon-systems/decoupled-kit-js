7e8bc714283cd4d7cb522410746d5008a90fb8e2
CobyPear <coby.sher@pantheon.io>
2022-11-08 15:34:45 -0600

    DB-4593: Update GitHub user & email in script

--- .github/scripts/with-commit.sh
+++ .github/scripts/with-commit.sh
@@ -1,4 +1,4 @@
-git config --global user.email "bot@getpantheon.com"
-git config --global user.name "Pantheon Automation"
-git commit -am "[skip ci] $COMMIT_MESSAGE on $(date +"%D at %T %Z")"
+git config --global user.email "decoupled-service-user@pantheon.io"
+git config --global user.name "pantheon-decoupled-service-user"
+git commit -am "[skip ci] $COMMIT_MSG on $(date +"%D at %T %Z")"
 git push
