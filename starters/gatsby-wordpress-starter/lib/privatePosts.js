1b57bdad3ec652cae7b282b39b4922d92c1b45e9
mitchellmarkoff <mitchell.markoff@pantheon.io>
2022-11-23 12:37:28 -0800

    DB-1406: CR Revisions

--- starters/gatsby-wordpress-starter/lib/privatePosts.js
+++ starters/gatsby-wordpress-starter/lib/privatePosts.js
@@ -1,6 +1,6 @@
 const { gql, GraphqlClientFactory } = require('@pantheon-systems/wordpress-kit')
 
-const client = new GraphqlClientFactory(`${process.env.WPGRAPHQL_URL}`).create()
+const client = new GraphqlClientFactory(process.env.WPGRAPHQL_URL).create()
 
 async function privatePostsQuery() {
 	const credentials = `${process.env.WP_APPLICATION_USERNAME}:${process.env.WP_APPLICATION_PASSWORD}`
