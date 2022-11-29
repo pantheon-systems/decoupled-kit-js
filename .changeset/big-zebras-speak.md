6e00a980cd9a98d8f596730867a226b43b9afe10
enZane <thunderstudios2@gmail.com>
2022-11-29 11:47:32 -0600

    DB-4829: Fix image width

---
+++ .changeset/big-zebras-speak.md
@@ -0,0 +1,5 @@
+---
+'@pantheon-systems/wordpress-kit': minor
+---
+
+Use max-width to images
--- packages/wordpress-kit/src/lib/tailwindcssPlugin/components/Image.ts
+++ packages/wordpress-kit/src/lib/tailwindcssPlugin/components/Image.ts
@@ -9,7 +9,7 @@ export const ImageComponent = () => ({
 			wordBreak: 'break-word',
 		},
 		img: {
-			width: '100%',
+			'max-width': '100%',
 			borderRadius: 'inherit',
 		},
 		'&.alignleft': {
