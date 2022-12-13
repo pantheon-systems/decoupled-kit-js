fa97a58da03215d1e3f9be27bba5209df3eb2783
CobyPear <coby.sher@pantheon.io>
2022-12-13 11:30:10 -0600

    DB-883: [wordpress-kit] use Pantheon-SKey header instead of Pantheon-Debug

---
+++ .changeset/angry-rules-knock.md
@@ -0,0 +1,6 @@
+---
+'@pantheon-systems/wordpress-kit': minor
+---
+
+clients created with `GraphqlClientFactory` now set the `Pantheon-SKey` header
+instead of `Pantheon-Debug`
--- packages/wordpress-kit/__mocks__/server/basicPostsQuery.ts
+++ packages/wordpress-kit/__mocks__/server/basicPostsQuery.ts
@@ -5,7 +5,7 @@ import basicPostsQueryData from '../../__tests__/data/basicPostsQuery.json';
 export const basicPostsQuery = graphql.query<typeof basicPostsQueryData>(
 	'BasicPostsQuery',
 	(req, res, ctx) => {
-		if (req.headers.has('pantheon-debug')) {
+		if (req.headers.has('Pantheon-SKey')) {
 			return res(
 				ctx.data(basicPostsQueryData),
 				ctx.set('Surrogate-Key-Raw', 'post-7 post-1 user-1 graphql-collection'),
