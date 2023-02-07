c3d91882035d5474859537f2fefe8b35c04086a0
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-02-07 12:08:41 -0800

    DB-5052: CR Revisions

--- packages/create-pantheon-decoupled-kit/__tests__/runESLint.test.ts
+++ packages/create-pantheon-decoupled-kit/__tests__/runESLint.test.ts
@@ -4,6 +4,7 @@ import whichPMRuns from 'which-pm-runs';
 import { execSync } from 'child_process';
 import type { ParsedArgs } from 'minimist';
 import type { CustomActionConfig } from 'node-plop';
+import * as fs from 'fs';
 
 vi.mock('node-plop');
 vi.mock('child_process');
@@ -12,15 +13,44 @@ vi.mock('which-pm-runs');
 const config: CustomActionConfig<'runLint'> = {
 	type: 'runLint',
 };
-const outDir = `${process.cwd()}/__tests__/fixtures/runESLint`;
-const answers: ParsedArgs = { _: [], outDir };
+const outDir = (dir: 'withLint' | 'withoutLint') =>
+	`${process.cwd()}/__tests__/fixtures/runESLint/${dir}`;
 const plop = await nodePlop.default();
 
 describe('runEsLint()', () => {
+	beforeEach(() => {
+		fs.writeFileSync(
+			`${outDir('withLint')}/package.json`,
+			JSON.stringify({
+				scripts: {
+					lint: 'next lint',
+				},
+			}),
+		);
+		fs.writeFileSync(
+			`${outDir('withoutLint')}/package.json`,
+			JSON.stringify({
+				scripts: {},
+			}),
+		);
+	});
 	afterEach(() => {
 		vi.resetAllMocks();
+
+		['package.json', 'node_modules'].forEach((path) => {
+			fs.rmSync(`${outDir('withLint')}/${path}`, {
+				force: true,
+				recursive: true,
+			});
+			fs.rmSync(`${outDir('withoutLint')}/${path}`, {
+				force: true,
+				recursive: true,
+			});
+		});
 	});
 	it('should lint and format the outDir using the detected package manager: pnpm', async () => {
+		const answers: ParsedArgs = { _: [], outDir: outDir('withLint') };
+
 		vi.mocked(whichPMRuns).mockImplementationOnce(() => ({
 			name: 'pnpm',
 			version: 'x',
@@ -29,12 +59,14 @@ describe('runEsLint()', () => {
 		actions.runESLint(answers, config, plop);
 		expect(vi.mocked(execSync)).toHaveBeenCalledWith('pnpm lint', {
 			stdio: 'inherit',
-			cwd: outDir,
+			cwd: outDir('withLint'),
 		});
 		expect(whichPMRuns).toHaveBeenCalledOnce();
 	});
 
 	it('should lint and format the outDir using the detected package manager: npm', async () => {
+		const answers: ParsedArgs = { _: [], outDir: outDir('withLint') };
+
 		vi.mocked(whichPMRuns).mockImplementationOnce(() => ({
 			name: 'npm',
 			version: 'x',
@@ -43,12 +75,29 @@ describe('runEsLint()', () => {
 		actions.runESLint(answers, config, plop);
 		expect(vi.mocked(execSync)).toHaveBeenCalledWith('npm run lint', {
 			stdio: 'inherit',
-			cwd: outDir,
+			cwd: outDir('withLint'),
+		});
+		expect(whichPMRuns).toHaveBeenCalledOnce();
+	});
+	it('should lint and format the outDir with no lint script installed', async () => {
+		const answers: ParsedArgs = { _: [], outDir: outDir('withoutLint') };
+
+		vi.mocked(whichPMRuns).mockImplementationOnce(() => ({
+			name: 'npm',
+			version: 'x',
+		}));
+
+		actions.runESLint(answers, config, plop);
+		expect(vi.mocked(execSync)).toHaveBeenCalledWith('npx eslint  ', {
+			stdio: 'inherit',
+			cwd: outDir('withoutLint'),
 		});
 		expect(whichPMRuns).toHaveBeenCalledOnce();
 	});
 
 	it.fails('should throw if there is an error', () => {
+		const answers: ParsedArgs = { _: [], outDir: outDir('withLint') };
+
 		const runESLintSpy = vi.spyOn(actions, 'runESLint');
 		vi.mocked(execSync).mockImplementationOnce(() => {
 			throw new Error('Some error happened');
@@ -59,6 +108,8 @@ describe('runEsLint()', () => {
 	});
 
 	it.fails('should throw if there is an error', () => {
+		const answers: ParsedArgs = { _: [], outDir: outDir('withLint') };
+
 		const runESLintSpy = vi.spyOn(actions, 'runESLint');
 		delete answers.outDir;
 
