728aaa3908cbc75e8ea2bb0a30698b372db4ffe0
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-02-21 10:53:32 -0800

    DB-5403: Related Content ACF Component Addon

--- packages/create-pantheon-decoupled-kit/src/generators/index.ts
+++ packages/create-pantheon-decoupled-kit/src/generators/index.ts
@@ -2,10 +2,12 @@ import { nextWp } from './next-wp.generator';
 import { nextDrupal } from './next-drupal.generator';
 import { gatsbyWp } from './gatsby-wp.generator';
 import { nextDrupalUmamiAddon } from './next-drupal-umami-addon.generator';
+import { nextWpAcfAddon } from './next-wp-acf-addon.generator';
 
 export const decoupledKitGenerators = [
 	nextWp,
 	gatsbyWp,
 	nextDrupal,
 	nextDrupalUmamiAddon,
+	nextWpAcfAddon,
 ];
