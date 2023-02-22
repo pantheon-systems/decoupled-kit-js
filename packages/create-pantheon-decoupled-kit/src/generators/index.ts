21f62a65c251cc45cf18911ee123b5d2a0b9204c
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-02-22 12:03:27 -0800

    DB-5404: Gatsby Related Content ACF Addon

--- packages/create-pantheon-decoupled-kit/src/generators/index.ts
+++ packages/create-pantheon-decoupled-kit/src/generators/index.ts
@@ -2,10 +2,12 @@ import { nextWp } from './next-wp.generator';
 import { nextDrupal } from './next-drupal.generator';
 import { gatsbyWp } from './gatsby-wp.generator';
 import { nextDrupalUmamiAddon } from './next-drupal-umami-addon.generator';
+import { gatsbyWpAcfAddon } from './gatsby-wp-acf-addon.generator';
 
 export const decoupledKitGenerators = [
 	nextWp,
 	gatsbyWp,
 	nextDrupal,
 	nextDrupalUmamiAddon,
+	gatsbyWpAcfAddon,
 ];
