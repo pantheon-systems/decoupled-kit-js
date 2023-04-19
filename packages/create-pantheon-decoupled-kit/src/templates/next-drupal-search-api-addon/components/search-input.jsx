005a74710d10964ce2a63ef117206a6d6868aa83
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-04-19 11:22:02 -0400

    DB-5758: Fix indentation

--- packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/components/search-input.jsx
+++ packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/components/search-input.jsx
@@ -22,15 +22,15 @@ const SearchInput = () => {
 		<form onSubmit={onSearch}>
 			<div className="relative flex w-full items-stretch">
 				<label  htmlFor="search">
-				<input
-				className="-mr-0.5 h-full rounded-l border border-solid border-neutral-300 bg-transparent px-3 text-base font-normal text-neutral-700 outline-none transition duration-200 ease-in-out focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)]"
-				type="search"
-				value={searchQuery || ''}
-				onChange={(event) => setSearchQuery(event.target.value)} 
-				placeholder="Search"
-				id="search"
-				aria-label="Search Bar"
-				/>
+					<input
+						className="-mr-0.5 h-full rounded-l border border-solid border-neutral-300 bg-transparent px-3 text-base font-normal text-neutral-700 outline-none transition duration-200 ease-in-out focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)]"
+						type="search"
+						value={searchQuery || ''}
+						onChange={(event) => setSearchQuery(event.target.value)} 
+						placeholder="Search"
+						id="search"
+						aria-label="Search Bar"
+					/>
 				</label>
 
 				<button
@@ -39,16 +39,16 @@ const SearchInput = () => {
 					id="submit-btn"
 					aria-label="Submit Search"
 				>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					className="h-5 w-5">
-					<path
-					fill-rule="evenodd"
-					d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-					clip-rule="evenodd" />
-				</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="h-5 w-5">
+						<path
+						fill-rule="evenodd"
+						d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+						clip-rule="evenodd" />
+					</svg>
 				</button>
 			</div>
 		</form>
