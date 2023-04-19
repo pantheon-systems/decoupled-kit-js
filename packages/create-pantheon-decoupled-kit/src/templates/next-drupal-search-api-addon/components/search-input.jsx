fe42a4526e6621dd9964bda122a55e3850d2ebd5
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-04-19 11:15:53 -0400

    DB-5758: Search Box Refinements

--- packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/components/search-input.jsx
+++ packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/components/search-input.jsx
@@ -20,24 +20,37 @@ const SearchInput = () => {
 
 	return (
 		<form onSubmit={onSearch}>
-			<label htmlFor="search">
+			<div className="relative flex w-full items-stretch">
+				<label  htmlFor="search">
 				<input
-					value={searchQuery || ''}
-					onChange={(event) => setSearchQuery(event.target.value)}
-					className="py-1 sm:px-5 text-black rounded-full focus:outline-none ring-[1px] ring-black"
-					placeholder="Search"
-					id="search"
-					aria-label="Search Bar"
+				className="-mr-0.5 h-full rounded-l border border-solid border-neutral-300 bg-transparent px-3 text-base font-normal text-neutral-700 outline-none transition duration-200 ease-in-out focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)]"
+				type="search"
+				value={searchQuery || ''}
+				onChange={(event) => setSearchQuery(event.target.value)} 
+				placeholder="Search"
+				id="search"
+				aria-label="Search Bar"
 				/>
-			</label>
-			<button
-				type="submit"
-				id="submit-btn"
-				className="ml-1.5 text-black rounded-full ring-[1px] ring-black px-5 py-1 hover:bg-blue-500"
-				aria-label="Submit Search"
-			>
-				Submit
-			</button>
+				</label>
+
+				<button
+					className="relative flex items-center rounded-r !bg-blue-600 px-6 py-2.5 text-white shadow-md hover:!bg-blue-700 hover:shadow-lg focus:!bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:!bg-blue-800 active:shadow-lg"
+					type="submit"
+					id="submit-btn"
+					aria-label="Submit Search"
+				>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					className="h-5 w-5">
+					<path
+					fill-rule="evenodd"
+					d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+					clip-rule="evenodd" />
+				</svg>
+				</button>
+			</div>
 		</form>
 	);
 };
