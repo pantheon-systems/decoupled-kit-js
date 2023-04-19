7f215056c4149e8eb17635668056a429634a6dc2
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-04-19 15:01:38 -0400

    DB-5758: Update Search Box Positioning

--- packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/components/search-input.jsx
+++ packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/components/search-input.jsx
@@ -45,9 +45,9 @@ const SearchInput = () => {
 						fill="currentColor"
 						className="h-5 w-5">
 						<path
-						fill-rule="evenodd"
+						fillRule="evenodd"
 						d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-						clip-rule="evenodd" />
+						clipRule="evenodd" />
 					</svg>
 				</button>
 			</div>
