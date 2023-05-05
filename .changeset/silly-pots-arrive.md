ea3c25b2444de849f076bf52065ccb066ac343c5
CobyPear <coby.sher@pantheon.io>
2023-05-05 11:37:43 -0500

    DB-6014: Set hashed surrogate keys in wordpress-kit

---
+++ .changeset/silly-pots-arrive.md
@@ -0,0 +1,5 @@
+---
+'create-pantheon-decoupled-kit': patch
+---
+
+[next-wp][gatsby-wp] Bump `wordpress-kit` version
--- packages/wordpress-kit/__mocks__/server/basicPostsQuery.ts
+++ packages/wordpress-kit/__mocks__/server/basicPostsQuery.ts
@@ -5,10 +5,10 @@ import basicPostsQueryData from '../../__tests__/data/basicPostsQuery.json';
 export const basicPostsQuery = graphql.query<typeof basicPostsQueryData>(
 	'BasicPostsQuery',
 	(req, res, ctx) => {
-		if (req.headers.has('Pantheon-SKey')) {
+		if (req.headers.has('Fastly-Debug')) {
 			return res(
 				ctx.data(basicPostsQueryData),
-				ctx.set('Surrogate-Key-Raw', 'post-7 post-1 user-1 graphql-collection'),
+				ctx.set('Surrogate-Key', 'post-7 post-1 user-1 graphql-collection'),
 			);
 		}
 		return res(ctx.data(basicPostsQueryData));
