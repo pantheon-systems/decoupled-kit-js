dffc487c9f3010a3cfda0c36986f8e4c04cd179d
CobyPear <coby.sher@pantheon.io>
2023-05-16 10:06:37 -0500

    DB-6039:
    - Specify eslint ignore files in cli .eslintrc
    - Add css modules for gatsby-wp-acf-addon
    - Make page & post widths consistent

--- packages/create-pantheon-decoupled-kit/src/generators/gatsby-wp-acf-addon.generator.ts
+++ packages/create-pantheon-decoupled-kit/src/generators/gatsby-wp-acf-addon.generator.ts
@@ -1,13 +1,22 @@
-import { addWithDiff, runLint } from '../actions';
-import type { DecoupledKitGenerator } from '../types';
-import { outDirPrompt } from '../utils/sharedPrompts';
+import { addWithDiff, convertCSSModules, runLint } from '../actions';
+import type { DecoupledKitGenerator, DefaultAnswers } from '../types';
+import { outDirPrompt, tailwindcssPrompt } from '../utils/sharedPrompts';
 
-export const gatsbyWpAcfAddon: DecoupledKitGenerator = {
-	name: 'gatsby-wp-acf-addon',
-	description:
-		'Example implementation of the WordPress Advanced Custom Fields plugin for the gatsby-wordpress starter',
-	prompts: [outDirPrompt(`${process.cwd()}/gatsby-wp-acf-addon`)],
-	templates: ['gatsby-wp-acf-addon'],
-	actions: [addWithDiff, runLint],
-	cmsType: 'wp',
-};
+interface GatsbyWpAcfAddonAnswers extends DefaultAnswers {
+	outDir: string;
+	tailwindcss: boolean;
+}
+
+export const gatsbyWpAcfAddon: DecoupledKitGenerator<GatsbyWpAcfAddonAnswers> =
+	{
+		name: 'gatsby-wp-acf-addon',
+		description:
+			'Example implementation of the WordPress Advanced Custom Fields plugin for the gatsby-wordpress starter',
+		prompts: [
+			outDirPrompt(`${process.cwd()}/gatsby-wp-acf-addon`),
+			tailwindcssPrompt,
+		],
+		templates: ['gatsby-wp-acf-addon'],
+		actions: [addWithDiff, convertCSSModules, runLint],
+		cmsType: 'wp',
+	};
