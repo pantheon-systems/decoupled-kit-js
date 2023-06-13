eae5813d096edab61ddc54126ea6345956362265
CobyPear <coby.sher@pantheon.io>
2023-06-13 10:46:38 -0500

    DB-5244:
    - Ignore linting for static templates
    - Fix memory leak in watch script

--- packages/create-pantheon-decoupled-kit/lint-staged.config.cjs
+++ packages/create-pantheon-decoupled-kit/lint-staged.config.cjs
@@ -1,6 +1,12 @@
+// TODO ensure only tagged templates are linted
+// but make sure other ts files are linted
 module.exports = {
-	'./src/templates/**/*.{ts.ts}': [
+	'./src/templates/**/*.ts.ts': [
 		`eslint --fix --config ./.eslintrc --ignore-pattern **/src/templates/**/*.{d.ts,ts,jsx,tsx}`,
+		`prettier --write --ignore-path ../../.prettierignore`,
+	],
+	'./src/**/*': [
+		`eslint --fix --config ./.eslintrc --ignore-pattern **/src/templates/**/*`,
 		`prettier --write --ignore-path ../../.prettierignore`,
 	],
 };
