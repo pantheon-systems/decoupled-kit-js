995621470b28a7032fa6a6ddf828043de39dbf63
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-07-24 15:15:01 -0400

    DB-6550: New line chars

--- packages/create-pantheon-decoupled-kit/src/generators/next-wp-search-api-addon.generator.ts
+++ packages/create-pantheon-decoupled-kit/src/generators/next-wp-search-api-addon.generator.ts
@@ -1,40 +1,40 @@
 import chalk from 'chalk';
 import {
-    addWithDiff,
-    convertCSSModules,
-    runInstall,
-    runLint,
+	addWithDiff,
+	convertCSSModules,
+	runInstall,
+	runLint,
 } from '../actions';
 import type { DecoupledKitGenerator, DefaultAnswers } from '../types';
 import { outDirPrompt, tailwindcssPrompt } from '../utils/sharedPrompts';
 
 interface NextWpSearchApiAddonAnswers {
-    search: true;
+	search: true;
 }
 
 export const nextWpSearchApiAddon: DecoupledKitGenerator<
-    DefaultAnswers,
-    NextWpSearchApiAddonAnswers
+	DefaultAnswers,
+	NextWpSearchApiAddonAnswers
 > = {
-    name: 'next-wp-search-api-addon',
-    description:
-        'Example implementation of the WordPress Search API for the next-wp starter',
-    prompts: [
-        outDirPrompt(`${process.cwd()}/next-wp-search-api-addon`),
-        tailwindcssPrompt,
-    ],
-    addon: true,
-    data: {
-        search: true,
-    },
-    templates: ['next-wp-search-api-addon', 'tailwind-shared'],
-    actions: [addWithDiff, runInstall, convertCSSModules, runLint],
-    nextSteps: [
-        `${chalk.cyan(
-            `Run the ${chalk.white.bold(
-                'update-snapshots',
-            )} command to update the snapshot files with the add-on components`,
-        )}`,
-    ],
-    cmsType: 'wp',
+	name: 'next-wp-search-api-addon',
+	description:
+		'Example implementation of the WordPress Search API for the next-wp starter',
+	prompts: [
+		outDirPrompt(`${process.cwd()}/next-wp-search-api-addon`),
+		tailwindcssPrompt,
+	],
+	addon: true,
+	data: {
+		search: true,
+	},
+	templates: ['next-wp-search-api-addon', 'tailwind-shared'],
+	actions: [addWithDiff, runInstall, convertCSSModules, runLint],
+	nextSteps: [
+		`${chalk.cyan(
+			`Run the ${chalk.white.bold(
+				'update-snapshots',
+			)} command to update the snapshot files with the add-on components`,
+		)}`,
+	],
+	cmsType: 'wp',
 };
