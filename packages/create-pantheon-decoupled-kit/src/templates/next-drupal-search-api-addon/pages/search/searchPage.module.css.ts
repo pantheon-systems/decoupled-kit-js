728ee11b0d4754d9186283950e2ae438b8163ee5
mitchellmarkoff <mitchell.markoff@pantheon.io>
2023-08-21 12:15:54 -0700

    DB-6547: next-wp Functioning Search Route

---
+++ packages/create-pantheon-decoupled-kit/src/templates/next-drupal-search-api-addon/pages/search/searchPage.module.css.ts
@@ -0,0 +1,6 @@
+import { TemplateFn } from '@cli/types';
+import { searchPageCSSModules } from '@partials/nextjs-shared/searchPageCSSModules';
+
+const css: TemplateFn = () => `${searchPageCSSModules()}`;
+
+export default css;
